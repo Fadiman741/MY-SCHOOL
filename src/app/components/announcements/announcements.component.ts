57697051f424c8cb489b20fc11ccb64435b19818
Mafadi Lephaka <mafadilephaka97@gmail.com>
2023-06-18 20:19:25 +0200

    update

--- src/app/components/announcements/announcements.component.ts
+++ src/app/components/announcements/announcements.component.ts
@@ -1,42 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
+
 @Component({
   selector: 'app-announcements',
   templateUrl: './announcements.component.html',
   styleUrls: ['./announcements.component.css']
 })
 export class AnnouncementsComponent implements OnInit {
-
   breadCrumbItems: Array<{}> = [];
   isReadMore: true = true;
+  announcements: any;
 
-  announcements : any;
-
-  constructor( private apiservice:ApiService) { }
+  constructor(private apiservice: ApiService) { }
 
   ngOnInit() {
-    this.breadCrumbItems = [{ label: 'Home' }, { label: 'Annoucement', active: true }];
-    
+    this.breadCrumbItems = [{ label: 'Home' }, { label: 'Announcement', active: true }];
     this.getAnnouncements();
   }
 
-  showText(){
+  showText() {
     this.isReadMore != this.isReadMore;
   }
+
   getAnnouncements = () => {
     this.apiservice.getAllAnnouncements().subscribe(
-
       data => {
-        this.announcements = data
+        this.announcements = data;
       },
       error => {
-        console.log(error)
+        console.log(error);
       }
-    )
+    );
 
-  console.log(this.announcements)
-  console.log("Hello world")
-
-
+    console.log(this.announcements);
+    console.log("Hello world");
   }
 }
